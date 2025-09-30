d78fe6482756f3688bc01cf88635118adad3c426
Vincent Chavez <VincentChavez7@icloud.com>
2025-09-30 02:12:05 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import random 
+st.header('Tossing a Coin')
+st.write('It is not a functional application yet. Under construction.')
+ls -a
