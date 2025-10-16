76f459f469bcdbb24a933546c8b28b46a67f38a6
Vincent Chavez <VincentChavez7@icloud.com>
2025-10-16 01:47:20 -0600

    Add experiment tracking with results table

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-import streamlit as st
 import pandas as pd
-import altair as alt
-import random
 import scipy.stats
+import streamlit as st
 import time
- 
+
+# these are stateful variables which are preserved as Streamlit reruns this script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
+
 st.header('Tossing a Coin')
 
 chart = st.line_chart([0.5])
@@ -14,7 +19,7 @@ def toss_coin(n):
     mean = None
     outcome_no = 0
     outcome_1_count = 0
-    
+
     for r in trial_outcomes:
         outcome_no +=1
         if r == 1:
@@ -22,7 +27,7 @@ def toss_coin(n):
         mean = outcome_1_count / outcome_no
         chart.add_rows([mean])
         time.sleep(0.05)
-    
+
     return mean
 
 number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
@@ -30,5 +35,20 @@ start_button = st.button('Run')
 
 if start_button:
     st.write(f'Running the experiment of {number_of_trials} trials.')
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = \
+        st.session_state['df_experiment_results'].reset_index(drop=True)
 
+st.write(st.session_state['df_experiment_results']) 
+
+
+    
