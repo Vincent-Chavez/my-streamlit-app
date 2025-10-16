8e0016dd412ee590f2b20800c3ca4b8b6b954407
Vincent Chavez <VincentChavez7@icloud.com>
2025-10-15 19:57:00 -0600

    Add slider and button widgets to coin flip app

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ import pandas as pd
 import altair as alt
 import random 
 st.header('Tossing a Coin')
+number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Run')
+
+if start_button:
+    st.write(f'Running the experiment of {number_of_trials} trials.')
 st.write('It is not a functional application yet. Under construction.')
-ls -a
+
